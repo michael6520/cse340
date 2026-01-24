ce2171f5f2227fa9819507e27461cf512356edb3
michael6520 <mm6520590@gmail.com>
2026-01-23 20:14:09 -0700

    Database

---
+++ database/assignment2.sql
@@ -0,0 +1,37 @@
+INSERT INTO public.account (
+    account_firstname,
+    account_lastname,
+    account_email,
+    account_password
+)
+VALUES (
+    'Tony',
+    'Stark',
+    'tony@starkent.com',
+    'Iam1ronM@n'
+);
+
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony';
+
+DELETE FROM public.account
+WHERE account_firstname = 'Tony';
+
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_model = 'Hummer';
+
+SELECT
+    i.make,
+    i.model,
+    c.classification_name
+FROM public.inventory i
+INNER JOIN public.classification c
+    ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+UPDATE public.inventory
+SET
+    inv_image = REPLACE(inv_image, 'images', 'images/vehicles')
+    inv_thumbnail = REPLACE(inv_thumbnail, 'images', 'images/vehicles');
